bcc9651a6985a251bce71bf97ebd155574466fb1
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-01-16 09:03:41 +0100

    using spies

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,22 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should set todos property with the items returned from the server', () => {
+    spyOn(service, 'getTodos').and.callFake(() => Observable.from([[1, 2, 3]]));
+
+    component.ngOnInit();
+
+    expect(component.todos.length).toBeGreaterThan(0);
   });
 });
